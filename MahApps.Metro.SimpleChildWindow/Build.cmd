10f24b7bb47249d4a7b6141542f7a1c4d55a607d
Jan Karger <punkerat76@gmail.com>
2015-07-17 00:16:02 +0200

    so it's alpha time: 1.3.0-ALPHA001

--- MahApps.Metro.SimpleChildWindow/Build.cmd
+++ MahApps.Metro.SimpleChildWindow/Build.cmd
@@ -1,7 +1,10 @@
 @echo on
 call "%VS120COMNTOOLS%vsvars32.bat"
 
-msbuild.exe /ToolsVersion:4.0 "MahApps.Metro.SimpleChildWindow.sln" /p:configuration=Debug /t:Clean,Rebuild
 msbuild.exe /ToolsVersion:4.0 "MahApps.Metro.SimpleChildWindow.sln" /p:configuration=Release /t:Clean,Rebuild
-
 .nuget\NuGet.exe pack %~dp0MahApps.Metro.SimpleChildWindow.nuspec -OutputDirectory %~dp0
+
+msbuild.exe /ToolsVersion:4.0 "MahApps.Metro.SimpleChildWindow.sln" /p:configuration=Debug /t:Clean,Rebuild
+.nuget\NuGet.exe pack %~dp0MahApps.Metro.SimpleChildWindow.ALPHA.nuspec -OutputDirectory %~dp0
+
+pause
