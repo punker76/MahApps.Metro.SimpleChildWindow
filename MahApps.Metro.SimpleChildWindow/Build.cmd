6088824734e0591ff45e1ef04303cf14664c3ab0
Jan Karger <punkerat76@gmail.com>
2016-01-17 00:16:39 +0100

    :pushpin: v1.3.0

--- MahApps.Metro.SimpleChildWindow/Build.cmd
+++ MahApps.Metro.SimpleChildWindow/Build.cmd
@@ -1,10 +1,10 @@
 @echo on
-call "%VS120COMNTOOLS%vsvars32.bat"
+REM call "%VS120COMNTOOLS%vsvars32.bat"
 
-msbuild.exe /ToolsVersion:4.0 "MahApps.Metro.SimpleChildWindow.sln" /p:configuration=Release /t:Clean,Rebuild
+REM msbuild.exe /ToolsVersion:4.0 "MahApps.Metro.SimpleChildWindow.sln" /p:configuration=Release /t:Clean,Rebuild
 .nuget\NuGet.exe pack %~dp0MahApps.Metro.SimpleChildWindow.nuspec -OutputDirectory %~dp0
 
-msbuild.exe /ToolsVersion:4.0 "MahApps.Metro.SimpleChildWindow.sln" /p:configuration=Debug /t:Clean,Rebuild
-.nuget\NuGet.exe pack %~dp0MahApps.Metro.SimpleChildWindow.ALPHA.nuspec -OutputDirectory %~dp0
+REM msbuild.exe /ToolsVersion:4.0 "MahApps.Metro.SimpleChildWindow.sln" /p:configuration=Debug /t:Clean,Rebuild
+REM .nuget\NuGet.exe pack %~dp0MahApps.Metro.SimpleChildWindow.ALPHA.nuspec -OutputDirectory %~dp0
 
 pause
