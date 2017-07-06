af47e78a0dcc9c3c4610b886d9c9a72047b3ac86
punker76 <punkerat76@gmail.com>
2017-07-07 00:31:33 +0200

    add build tools update build cmd

--- MahApps.Metro.SimpleChildWindow/Build.cmd
+++ MahApps.Metro.SimpleChildWindow/Build.cmd
@@ -7,12 +7,7 @@ IF NOT "%VS140COMNTOOLS%" == "" (call "%VS140COMNTOOLS%vsvars32.bat")
 .paket\paket.bootstrapper.exe
 .paket\paket.exe restore
 
-msbuild.exe /ToolsVersion:14.0 "MahApps.Metro.SimpleChildWindow.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
-msbuild.exe /ToolsVersion:14.0 "MahApps.Metro.SimpleChildWindow.sln" /p:configuration=Release /p:platform="Any CPU" /m /t:Clean,Rebuild
+msbuild.exe /ToolsVersion:14.0 "..\MahApps.Metro.SimpleChildWindow\MahApps.Metro.SimpleChildWindow.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
+msbuild.exe /ToolsVersion:14.0 "..\MahApps.Metro.SimpleChildWindow\MahApps.Metro.SimpleChildWindow.sln" /p:configuration=Release /p:platform="Any CPU" /m /t:Clean,Rebuild
 
-@echo off
-
-REM .nuget\NuGet.exe pack %~dp0MahApps.Metro.SimpleChildWindow.nuspec -OutputDirectory %~dp0
-REM .nuget\NuGet.exe pack %~dp0MahApps.Metro.SimpleChildWindow.ALPHA.nuspec -OutputDirectory %~dp0
-
-pause
+..\BuildTools\NuGet.exe pack MahApps.Metro.SimpleChildWindow.ALPHA.nuspec -OutputDirectory %~dp0
