69bc3a1ec908849401a9642bbb3309b7369d0fcc
punker76 <punkerat76@gmail.com>
2017-07-19 22:39:00 +0200

    set v1.4.0

--- MahApps.Metro.SimpleChildWindow/MahApps.Metro.SimpleChildWindow/Properties/AssemblyInfo.cs
+++ MahApps.Metro.SimpleChildWindow/MahApps.Metro.SimpleChildWindow/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.3.2.0")]
-[assembly: AssemblyFileVersion("1.3.2.0")]
-[assembly: AssemblyInformationalVersion("1.3.2.0")]
-[assembly: AssemblyProduct("MahApps.Metro.SimpleChildWindow 1.3.2")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyInformationalVersion("1.4.0.0")]
+[assembly: AssemblyProduct("MahApps.Metro.SimpleChildWindow 1.4.0")]
