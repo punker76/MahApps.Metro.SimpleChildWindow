de2e092e3eaebd3d9fe7498ccbcc08bd8d96292c
punker76 <punkerat76@gmail.com>
2017-07-19 22:46:56 +0200

    GitLink for debug

--- MahApps.Metro.SimpleChildWindow/Build.cmd
+++ MahApps.Metro.SimpleChildWindow/Build.cmd
@@ -8,6 +8,7 @@ IF NOT "%VS140COMNTOOLS%" == "" (call "%VS140COMNTOOLS%vsvars32.bat")
 .paket\paket.exe restore
 
 msbuild.exe /ToolsVersion:14.0 "..\MahApps.Metro.SimpleChildWindow\MahApps.Metro.SimpleChildWindow.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
-msbuild.exe /ToolsVersion:14.0 "..\MahApps.Metro.SimpleChildWindow\MahApps.Metro.SimpleChildWindow.sln" /p:configuration=Release /p:platform="Any CPU" /m /t:Clean,Rebuild
+
+..\BuildTools\GitLink.exe . -u https://github.com/punker76/MahApps.Metro.SimpleChildWindow -b dev -c Debug -f MahApps.Metro.SimpleChildWindow.sln -ignore MahApps.Metro.SimpleChildWindow.Demo
 
 ..\BuildTools\NuGet.exe pack MahApps.Metro.SimpleChildWindow.ALPHA.nuspec -OutputDirectory %~dp0
