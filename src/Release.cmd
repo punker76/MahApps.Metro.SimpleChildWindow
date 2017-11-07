8de4b8bd01a95119fafb3388d4c70c3d288ddd4f
Jan Karger <punkerat76@gmail.com>
2017-11-07 22:51:07 +0100

    Prepare new version   use appveyor config file   use GitVersion   use GitLink via NuGet package

--- src/Release.cmd
+++
@@ -1,5 +0,0 @@
-@echo on
-
-..\build\GitLink.exe . -u https://github.com/punker76/MahApps.Metro.SimpleChildWindow -b master -c Release -f MahApps.Metro.SimpleChildWindow.sln -ignore MahApps.Metro.SimpleChildWindow.Demo
-
-..\build\NuGet.exe pack MahApps.Metro.SimpleChildWindow.nuspec -OutputDirectory %~dp0
